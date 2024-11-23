f1add9fbe6ff684a563e00802641e4b81e4ccb93
sueputhearathblue <138637018+sueputhearathblue@users.noreply.github.com>
2024-11-23 19:49:22 +0700

    fix

--- resources/views/Test/register.blade.php
+++ resources/views/Test/register.blade.php
@@ -1,26 +1,20 @@
 @extends("layout.master")
 @section("content")
 <div class="login-box">
-        <h2>Login</h2>
-        <form action="{{route("register.save")}}" method ="POST">
-            @csrf
-          <div class="user-box">
+    <h2>Login</h2>
+    <form action="{{route("register.save")}}" method="POST">
+        @csrf
+        <div class="user-box">
             <input type="text" name="username" required="">
             <label>Username</label>
-          </div>
-          <div class="user-box">
+        </div>
+        <div class="user-box">
             <input type="password" name="password" required="">
             <label>Password</label>
-          </div>
-          <button class="my-button" type="submit">SUBMIT</button>
-        </form>
-    
-      </div>
-    
-    {{-- <form>
-        <label for="fname">First name:</label><br>
-        <input type="text" id="fname" name="fname"><br>
-        <label for="lname">Last name:</label><br>
-        <input type="text" id="lname" name="lname">
-      </form> --}}
+        </div>
+        <button class="my-button" type="submit">SUBMIT</button>
+    </form>
+
+</div>
+
 @endsection
